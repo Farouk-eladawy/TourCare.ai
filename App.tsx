41e162c2f4a8cb67ba5aa3c457f3c7cd8d6d3eb3
Farouk-eladawy <eladawy522@gmail.com>
2025-10-21 01:42:59 +0300

    fix: Update video URL in VideoModal
    The video URL for the VideoModal component has been updated to point to a new version of the promotional video.

--- App.tsx
+++ App.tsx
@@ -109,7 +109,7 @@ function App() {
       <VideoModal 
         isOpen={isVideoModalOpen}
         onClose={() => setIsVideoModalOpen(false)}
-        videoUrl="https://res.cloudinary.com/dqlurfwet/video/upload/v1760998006/Recording_2025-10-20_183542_sqmt8o.mp4"
+        videoUrl="https://res.cloudinary.com/dqlurfwet/video/upload/v1761000090/Recording_2025-10-21_013913_n4ld5v.mp4"
       />
 
       <AuthModal
